2a7bbcf07db44146c63b3b1e1418874b0723b338
Yawmus <peterj@USA-IT-CXG8WM.local>
2019-06-05 20:32:28 -0600

    Fixed shader

--- Assets/GridScript.cs
+++ Assets/GridScript.cs
@@ -2,87 +2,134 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using System;
 
-public class GridScript : MonoBehaviour {
-
-	public int WIDTH = 10, HEIGHT = 10;
-	public int[,] grid;
-
-	public List<Particle> particles;
-	public ComputeShader shader;
-	Texture2D tex;
-	ComputeBuffer buffer;
-
-	// Use this for initialization
-	void Start () {
-		particles = new List<Particle> ();
-		for (int i = 0; i < WIDTH * HEIGHT; i++)
-		{
-			int x = i % WIDTH;
-			int y = i / HEIGHT;
-			Debug.Log ("x:" + x + ", y:" + y);
-			particles.Add (new Particle (x, y));
-		}
-
-
-		tex = new Texture2D (WIDTH, HEIGHT);
-		//pixels = tex.GetPixels ();
-		tex.filterMode = FilterMode.Point;
-//		tex = Instantiate(GetComponent<Renderer> ().material.mainTexture) as Texture2D;
-		GetComponent<Renderer> ().material.mainTexture = tex;
-	}
-
-	// Update is called once per frame
-	void Update () {
-		//pixels = tex.GetPixels ();
-//		for (int i = 0; i < WIDTH * HEIGHT; i++)
-//		{
-//			int x = i % WIDTH;
-//			int y = i / HEIGHT;
-//			//particles[WIDTH * y + x ] = new Color(grid[x, y], 1, 1);
-//			tex.SetPixel(i % WIDTH, y, new Color(grid[x, y], 1, 1));
-//		}
-		RunShader ();
-		tex.SetPixels (Particle.Colors.ToArray());
-		tex.Apply ();
-	}
-
-	void RunShader()
-	{
-		Vector3[] output = new Vector3[Particle.Positions.Count];
-		buffer = new ComputeBuffer(Particle.Positions.Count, 12);
-		//INITIALIZE DATA HERE
-
-		buffer.SetData(Particle.Positions.ToArray());
-		int kernel = shader.FindKernel("Move");
-		shader.SetBuffer(kernel, "dataBuffer", buffer);
-		shader.Dispatch(kernel, Particle.Positions.Count, 1, 1);
-		buffer.GetData(output);
-
-
-		Particle.SetPositions (output [i]);
-		Particle.Positions = Particle.Positions.OrderBy (i => i.y).ThenBy (i => i.x).ToList ();
-		buffer.Dispose ();
-	}
-}
-
-public class Particle
+public class GridScript : MonoBehaviour
 {
-	public static List<Vector3> Positions = new List<Vector3>();
-	static List<Color> Colors = new List<Color>();
-	public Color tint;
-	public Particle(int x, int y)
-	{
-		Color color = new Color (Random.Range (0, 1f), Random.Range (0, 1f), Random.Range (0, 1f));
-		Vector3 pos = new Vector3 (x, y, 0);
-		Positions.Add (pos);
-		Colors.Add (color);
-	}
-	public static void SetPositions(Vector3[] input)
-	{
-		for (int i=0; i<input.Length; i++)
-		{
-			Positions[i] = input[i];
-		}
-	}
+
+    public int WIDTH = 20, HEIGHT = 20;
+    public List<int> ids;
+
+    public Dictionary<int, Particle> particles;
+    public ComputeShader shader;
+    Texture2D tex;
+    ComputeBuffer buffer;
+    public struct Particle
+    {
+        public Color color;
+        public Vector3 position;
+    }
+    public struct ComputeData {
+        public Vector3 position;
+        public int id;
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        ids = new List<int>();
+        particles = new Dictionary<int, Particle>();
+        for (int i = 0; i < WIDTH * HEIGHT; i++)
+        {
+            int x = i % WIDTH;
+            int y = i / HEIGHT;
+
+            //AddParticle(x, y);
+        }
+
+
+        tex = new Texture2D(WIDTH, HEIGHT);
+        //pixels = tex.GetPixels ();
+        tex.filterMode = FilterMode.Point;
+        //		tex = Instantiate(GetComponent<Renderer> ().material.mainTexture) as Texture2D;
+        GetComponent<Renderer>().material.mainTexture = tex;
+    }
+
+    void AddParticle(int x, int y)
+    {
+        int id = UnityEngine.Random.Range(1, 20000);
+        Vector3 position = new Vector3(x, y, 0);
+        Color color = new Color(UnityEngine.Random.Range(0, 1f), UnityEngine.Random.Range(0, 1f), UnityEngine.Random.Range(0, 1f), 1);
+        particles[id] = new Particle { position = position, color = color };
+        ids.Add(id);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        AddParticle(UnityEngine.Random.Range(0, WIDTH), HEIGHT - 1);
+
+
+        Color[] pixels = new Color[WIDTH * HEIGHT];
+        for (int i = 0; i < WIDTH * HEIGHT; i++)
+        {
+            pixels[i] = new Color(0, 0, 0, 1);
+        }
+        for (int i = 0; i < ids.Count; i++)
+        {
+            int x = i % WIDTH;
+            int y = i / HEIGHT;
+            int id = ids[i];
+
+            Vector3 t = particles[id].position;
+            //Debug.Log("x:" + x + ", y:" + y);
+            pixels[WIDTH * (int)t.y + (int)t.x] = particles[id].color;
+        }
+
+        //pixels[2] = new Color(1, 1, 1, 1);
+
+        //Particle.Positions = g.OrderBy(i => i.y).ThenBy(i => i.x).ToList();
+        //tex.SetPixel(0, 0, Color.cyan);
+        if (ids.Count != 0)
+        {
+            RunShader();
+        }
+        tex.SetPixels(pixels);
+        tex.Apply();
+    }
+
+    void RunShader()
+    {
+        List<ComputeData> input = new List<ComputeData>();
+        for (int i = 0; i < ids.Count; i++)
+        {
+            int x = i % WIDTH;
+            int y = i / HEIGHT;
+            int id = ids[i];
+
+            Particle p = particles[id];
+            ComputeData temp = new ComputeData { position = p.position, id = id};
+            input.Add(temp);
+        }
+        ComputeData[] output = new ComputeData[input.Count];
+        buffer = new ComputeBuffer(input.Count, 16);
+        //INITIALIZE DATA HERE
+
+        buffer.SetData(input.ToArray());
+        int kernel = shader.FindKernel("Move");
+        shader.SetBuffer(kernel, "dataBuffer", buffer);
+        shader.SetInt("width", WIDTH);
+        shader.SetInt("height", HEIGHT);
+        shader.Dispatch(kernel, input.Count, 1, 1);
+        buffer.GetData(output);
+
+        for (int i = 0; i < output.Length; i++)
+        {
+            ComputeData data = output[i];
+            Particle p = particles[data.id];
+            if(data.position.y < 0)
+            {
+                particles.Remove(data.id);
+                ids.Remove(data.id);
+            }
+            else
+            {
+                particles[data.id] = new Particle { position = data.position, color = p.color };
+            }
+        }
+        buffer.Dispose();
+    }
 }
