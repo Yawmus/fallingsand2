dd55fe0ebdeeefab83f6eed52c844f4f8cd133bc
yawmus <peter.jacobsen55@gmail.com>
2019-06-11 22:50:09 -0600

    Update -- still broken Leetcode

--- Assets/Runner.cs
+++ Assets/Runner.cs
@@ -6,40 +6,58 @@ using UnityEngine;
 public class Runner : MonoBehaviour
 {
     public ComputeShader shader;
-    public List<uint> inputs;
 
     // Start is called before the first frame update
     void Start()
     {
-        List<uint> vals = new List<uint>();
-        for(int i=0; i < Mathf.Pow(inputs.Count, 2); i++)
+        uint[] test = { 1, 2, 3, 0, 2, 1, 2, 3, 0, 7 };
+        List<uint[]> tests = new List<uint[]>();
+        tests.Add(test);
+
+        for(int k=0; k<tests.Count; k++)
         {
-            vals.Add((uint)i);
+            System.Diagnostics.Stopwatch stopWatch = new System.Diagnostics.Stopwatch();
+            List<uint> vals = new List<uint>();
+            uint[] inputs = tests[k];
+
+            for (int i=0; i < Mathf.Pow(inputs.Length, 2); i++)
+            {
+                vals.Add((uint)i);
+            }
+
+            // Extra dimensions for padding
+            ComputeBuffer inputBuffer = new ComputeBuffer(inputs.Length, 4);
+            ComputeBuffer valBuffer = new ComputeBuffer(vals.Count, 4);
+            int[] outputs = new int[vals.Count];
+
+            int kernel = shader.FindKernel("CSMain");
+            inputBuffer.SetData(inputs);
+            valBuffer.SetData(vals);
+            shader.SetBuffer(kernel, "inputs", inputBuffer);
+            shader.SetBuffer(kernel, "vals", valBuffer);
+
+            stopWatch.Start();
+            shader.Dispatch(kernel, vals.Count > 1024 ? 1024 : vals.Count, 1, 1);
+            valBuffer.GetData(outputs);
+
+            for (int i = 0; i < vals.Count; i++)
+            {
+                //Debug.Log(outputs[i]); ;
+            }
+
+            int max = outputs.Max();
+            stopWatch.Stop();
+            Debug.Log(max);
+            Debug.Log(  "Time: " + stopWatch.ElapsedMilliseconds + "ms");
+
+            inputBuffer.Dispose();
+            valBuffer.Dispose();
         }
-
-        // Extra dimensions for padding
-        ComputeBuffer inputBuffer = new ComputeBuffer(inputs.Count, 4);
-        ComputeBuffer valBuffer = new ComputeBuffer(vals.Count, 4);
-        int[] outputs = new int[vals.Count];
-
-        int kernel = shader.FindKernel("CSMain");
-
-        inputBuffer.SetData(inputs);
-        valBuffer.SetData(vals);
-        shader.SetBuffer(kernel, "inputs", inputBuffer);
-        shader.SetBuffer(kernel, "vals", valBuffer);
-        shader.Dispatch(kernel, vals.Count, 1, 1);
-        valBuffer.GetData(outputs);
-
-        inputBuffer.Dispose();
-        valBuffer.Dispose();
-
-        Debug.Log(outputs.Max());
     }
-
-    // Update is called once per frame
-    void Update()
+    public static void DumpToConsole(object obj)
     {
-        
+        var output = JsonUtility.ToJson(obj, true);
+        Debug.Log(output);
     }
+
 }
